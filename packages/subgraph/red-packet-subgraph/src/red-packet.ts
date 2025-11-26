8569263c37ee25a3f1bd62632a0bf2c833e6660f
Katherine <celery8911@gmail.com>
2025-11-26 23:20:06 +0800

    feat: optimize subgraph schema and mapping

--- packages/subgraph/red-packet-subgraph/src/red-packet.ts
+++ packages/subgraph/red-packet-subgraph/src/red-packet.ts
@@ -1,37 +1,41 @@
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
   PacketClaimed as PacketClaimedEvent,
   PacketCreated as PacketCreatedEvent
 } from "../generated/RedPacket/RedPacket"
-import { PacketClaimed, PacketCreated } from "../generated/schema"
-
-export function handlePacketClaimed(event: PacketClaimedEvent): void {
-  let entity = new PacketClaimed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.internal_id = event.params.id
-  entity.claimer = event.params.claimer
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import { Packet, Claim } from "../generated/schema"
 
 export function handlePacketCreated(event: PacketCreatedEvent): void {
-  let entity = new PacketCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.internal_id = event.params.id
-  entity.creator = event.params.creator
-  entity.totalAmount = event.params.totalAmount
-  entity.count = event.params.count
-  entity.packetType = event.params.packetType
+  let packet = new Packet(event.params.id)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  packet.creator = event.params.creator
+  packet.totalAmount = event.params.totalAmount
+  packet.remainingAmount = event.params.totalAmount
+  packet.count = event.params.count
+  packet.remainingCount = event.params.count
+  packet.packetType = event.params.packetType
+  packet.timestamp = event.block.timestamp
+  packet.txHash = event.transaction.hash
 
-  entity.save()
+  packet.save()
+}
+
+export function handlePacketClaimed(event: PacketClaimedEvent): void {
+  let packet = Packet.load(event.params.id)
+  if (packet) {
+    packet.remainingAmount = packet.remainingAmount.minus(event.params.amount)
+    packet.remainingCount = packet.remainingCount.minus(BigInt.fromI32(1))
+    packet.save()
+
+    let claimId = event.transaction.hash.concatI32(event.logIndex.toI32())
+    let claim = new Claim(claimId)
+
+    claim.packet = packet.id
+    claim.claimer = event.params.claimer
+    claim.amount = event.params.amount
+    claim.timestamp = event.block.timestamp
+    claim.txHash = event.transaction.hash
+
+    claim.save()
+  }
 }
