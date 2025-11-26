92982bdbd08443b0c2a01b83be3b169c65c9b956
Katherine <celery8911@gmail.com>
2025-11-27 01:24:56 +0800

    feat: rebuild frontend with pnpm and rainbowkit, add urql and components

--- packages/frontend/red-packet-frontend/src/wagmi.ts
+++ packages/frontend/red-packet-frontend/src/wagmi.ts
@@ -11,13 +11,6 @@ import {
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
+  chains: [sepolia],
   ssr: true,
 });
